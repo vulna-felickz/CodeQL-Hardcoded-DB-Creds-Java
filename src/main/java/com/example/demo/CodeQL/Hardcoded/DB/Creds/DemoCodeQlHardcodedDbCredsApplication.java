053d4e24269b77de49478a33f707ea3604f99503
Chad Bentz <1760475+felickz@users.noreply.github.com>
2023-02-01 15:03:19 -0500

    initial

---
+++ src/main/java/com/example/demo/CodeQL/Hardcoded/DB/Creds/DemoCodeQlHardcodedDbCredsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.CodeQL.Hardcoded.DB.Creds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCodeQlHardcodedDbCredsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCodeQlHardcodedDbCredsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/CodeQL/Hardcoded/DB/Creds/DemoCodeQlHardcodedDbCredsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.CodeQL.Hardcoded.DB.Creds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCodeQlHardcodedDbCredsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
