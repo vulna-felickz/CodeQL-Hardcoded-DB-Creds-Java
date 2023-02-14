d0c926ab4e46efecc0a399db814eee42b8e52d59
Chad Bentz <1760475+felickz@users.noreply.github.com>
2023-02-13 20:20:04 -0500

    Add MSSQL Scenarios

--- src/main/java/com/example/demo/CodeQL/Hardcoded/DB/Creds/CredentialsTestMsSql.java
+++ src/main/java/com/example/demo/CodeQL/Hardcoded/DB/Creds/CredentialsTestMsSql.java
@@ -3,6 +3,8 @@ package com.example.demo.CodeQL.Hardcoded.DB.Creds;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Driver;
+import java.util.Properties;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
 // https://learn.microsoft.com/en-us/sql/connect/jdbc/working-with-a-connection?view=sql-server-ver16
@@ -12,6 +14,21 @@ public class CredentialsTestMsSql {
 	public CredentialsTestMsSql() {
     }
 
+	// Driver Manager: https://learn.microsoft.com/en-us/sql/connect/jdbc/working-with-a-connection?view=sql-server-ver16#creating-a-connection-by-using-the-drivermanager-class
+	public static void testhardcoded() throws Exception {
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
+		String connectionUrl = "jdbc:sqlserver://localhost;encrypt=true;database=AdventureWorks";
+		Connection con = DriverManager.getConnection(connectionUrl, "user-mssql", sensitive); 
+	}
+
+	// SQLServerDriver https://learn.microsoft.com/en-us/sql/connect/jdbc/working-with-a-connection?view=sql-server-ver16#creating-a-connection-by-using-the-sqlserverdriver-class
+	public static void testhardcoded0() throws Exception {
+		Driver d = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();  
+		String connectionUrl = "jdbc:sqlserver://localhost;encrypt=true;database=AdventureWorks;user=user-mssql-0;password=password";  
+		Connection con = d.connect(connectionUrl, new Properties());
+	}
+
+	// SQLServerDataSource: https://learn.microsoft.com/en-us/sql/connect/jdbc/working-with-a-connection?view=sql-server-ver16#creating-a-connection-by-using-the-sqlserverdatasource-class
     public static void testhardcoded1() throws SQLException {
 		SQLServerDataSource ds = new SQLServerDataSource();  
 		ds.setUser("MyUserName-mssql-1");  
